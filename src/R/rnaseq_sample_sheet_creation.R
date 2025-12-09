b776a625477a6824df9b2e4da03e214dc9824152
nicolasDelhomme <nicolas.delhomme@umu.se>
2025-12-09 15:08:53 +0100

    Added a sample creation file and a sample file

--- src/R/rnaseq_sample_sheet_creation.R
+++ src/R/rnaseq_sample_sheet_creation.R
@@ -1,6 +1,21 @@
+library(dplyr)
 library(here)
-files<-list.files(here("data/raw"), pattern="*_1.fq.gz", full.names=TRUE)
-readr::write_csv(tibble::tibble(sample=sub("_1\\.fq\\.gz","",basename(files)),
-                                fastq_1=files,
-                                fastq_2=sub("_1\\.f","_2.f",files),
-                                strandedness="auto"),file="doc/sample_sheet.csv")
+files<-sort(list.files(here("data/raw"), pattern="*.fastq.gz", full.names=TRUE))
+md5 <- readr::read_table("data/raw/MD5.txt",
+                         col_names=c("MD5","file"),
+                         show_col_types=FALSE) %>%
+  mutate(file=basename(file))
+
+readr::write_csv(tibble::tibble(
+  ExperimentTitle="Aspen tension wood development gradient",
+  SampleName=sub("_[1,2].fastq.gz","",basename(files)),
+  SampleDescription="",
+  SequencingDate=sprintf("20%s-%s-%sT10:00:00",
+                         substr(basename(files),3,4),
+                         substr(basename(files),5,6),
+                         substr(basename(files),7,8)),
+  FileName=basename(files),
+  FileLocation=dirname(files),
+  MD5checksum=unlist(md5[match(basename(files),md5$file),"MD5"],use.names=FALSE)),
+file="doc/samples.csv")
+  
